d201bf0fdb68a563a5810d639ecbe36b0c11d8d1
Vivek-Saxena <vivek@example.com>
2023-08-24 16:09:18 +0530

    commit at 24-08-2023 at 16:09

--- erp_project/departments/models.py
+++ erp_project/departments/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Department(models.Model):
     name = models.CharField(max_length=100)
     ddescription = models.CharField(max_length=255, default='Default Description')
@@ -7,3 +8,12 @@ class Department(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class StudentResult(models.Model):
+    student_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.student_name} - {self.subject}"
--- erp_project/departments/views.py
+++ erp_project/departments/views.py
@@ -1,36 +1,41 @@
 from django.shortcuts import render
+from .models import StudentResult
 
 def index(request):
     return render(request, 'departments/adminkit/index.html')
 
-def profile(request):
-    return render(request, 'departments/adminkit/pages-profile.html')
+# def profile(request):
+#     return render(request, 'departments/adminkit/pages-profile.html')
 
-def signin(request):
-    return render(request, 'departments/adminkit/pages-sign-in.html')
+# def signin(request):
+#     return render(request, 'departments/adminkit/pages-sign-in.html')
 
-def signup(request):
-    return render(request, 'departments/adminkit/pages-sign-up.html')
+# def signup(request):
+#     return render(request, 'departments/adminkit/pages-sign-up.html')
 
-# Add more views for other HTML templates as needed
+# # Add more views for other HTML templates as needed
 
-def blank_page(request):
-    return render(request, 'departments/adminkit/pages-blank.html')
+# def blank_page(request):
+#     return render(request, 'departments/adminkit/pages-blank.html')
 
-def google_maps(request):
-    return render(request, 'departments/adminkit/maps-google.html')
+# def google_maps(request):
+#     return render(request, 'departments/adminkit/maps-google.html')
 
-def buttons(request):
-    return render(request, 'departments/adminkit/ui-buttons.html')
+# def buttons(request):
+#     return render(request, 'departments/adminkit/ui-buttons.html')
 
-def cards(request):
-    return render(request, 'departments/adminkit/ui-cards.html')
+# def cards(request):
+#     return render(request, 'departments/adminkit/ui-cards.html')
 
-def forms(request):
-    return render(request, 'departments/adminkit/ui-forms.html')
+# def forms(request):
+#     return render(request, 'departments/adminkit/ui-forms.html')
 
-def typography(request):
-    return render(request, 'departments/adminkit/ui-typography.html')
+# def typography(request):
+#     return render(request, 'departments/adminkit/ui-typography.html')
 
-def upgrade_to_pro(request):
-    return render(request, 'departments/adminkit/upgrade-to-pro.html')
+# def upgrade_to_pro(request):
+#     return render(request, 'departments/adminkit/upgrade-to-pro.html')
+
+def student_results(request):
+    results = StudentResult.objects.all()
+    return render(request, 'departments/adminkit/student_results.html', {'results': results})
